c3bd79dd370cedf3fbe9a60e951b374895570e13
Clinton Ecker <clint@arstechnica.com>
2009-04-25 10:58:39 -0500

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -7,16 +7,12 @@ if sys.version_info < (2, 5):
 import os
 from setuptools import setup, find_packages
 
-def read(fname):
-  return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
   name = "python-googleanalytics",
   version = "1.0",
   url = 'http://github.com/clintecker/python-googleanalytics',
   license = 'BSD',
   description = "A python library for talking to the Google Analytics API",
-  long_description = read('README.md'),
   
   author = 'Clint Ecker',
   author_email = 'me@clintecker.com',
